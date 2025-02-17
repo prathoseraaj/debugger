32fd5ac396803fbc698a36eacf027a8d0f164d22
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-02-17 22:53:32 +0530

    feat: listen

--- backend/server.js
+++ backend/server.js
@@ -67,3 +67,5 @@ io.on('connection',(socket)=>{
     socket.on('disconnect', () => console.log('User disconnected:', socket.id));
 
 });
+
+server.listen(5000, () => console.log('Server running on port 5000'));
