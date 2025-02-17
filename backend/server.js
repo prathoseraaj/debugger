0ff0eb7e04660f0aafb57183d0957a84eae70987
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-02-17 22:01:03 +0530

    backend sever require

---
+++ backend/server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const cors = require('cors');
+const {server} = require('server');
+const http = require('http');
+const dotenv = require('dotenv');
+const axios = require('axios');
